1c175d4864cb253859ab570f1dc31b248548cb5b
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-20 18:55:57 +0300

    feat:new branch+add new currency_converter

--- script/me.js
+++ script/me.js
@@ -1,70 +1,131 @@
-const  addCurrencyBtn = document.querySelector('.add-currency-btn'),
-addCyrrencyList=document.querySelector('.add-currency-list');
+let data = [{
+        base: 'USD',
+        rates: {
+            EUR: 0.85,
+            RUB: 34.456,
+            BYN: 2.345,
+        }
+    },
+    {
+        base: 'EUR',
+        rates: {
+            USD: 1.285,
+            RUB: 44.456,
+            BYN: 2.545,
+        }
+    },
+    {
+        base: 'RUB',
+        rates: {
+            USD: 0.00285,
+            EUR: 0.00156,
+            BYN: 0.0545,
+        }
+    },
+    {
+        base: 'BYN',
+        rates: {
+            USD: 0.585,
+            EUR: 0.4156,
+            RUB: 20.564,
+        }
+    },
+]
 
-const currencies = [
-  {
-    name: "US Dollar",
-    abbreviation: "USD",
-    symbol: "\u0024",
-    flagimg: '<img src="img/united_states.gif" alt="">',
-  },
-  {
-    name: "Euro",
-    abbreviation: "EUR",
-    symbol: "\u20AC",
-    flagimg: '<img src="img/euro.jpg.webp" alt="">'
-  },
-  {
-    name: "Australian Dollar",
-    abbreviation: "AUD",
-    symbol: "\u0024",
-    flagimg: '<img src="img/australia.gif" alt="">'
-  },
-  {
-    name: "Chinese Yuan Renminbi",
-    abbreviation: "CNY",
-    symbol: "\u00A5",
-    flagimg:'<img src="img/china.gif" alt="">'
-  },
-  {
-    name: "Singapore Dollar",
-    abbreviation: "SGD",
-    symbol: "\u0024",
-    flagimg: '<img src="img/singapore.gif" alt="">'
-  },
-  {
-    name: "Russian Ruble",
-    abbreviation: "RUB",
-    symbol: "\u20BD",
-    flagimg: '<img src="img/russia.gif" alt="">'
-  },
-  {
-    name: "Indonesian Rupiah",
-    abbreviation: "IDR",
-    symbol: "\u0052\u0070",
-    flagURL: '<img src="img/indonesia.gif" alt="">'
-  },
-  {
-    name: "Danish Krone",
-    abbreviation: "DKK",
-    symbol: "\u006B\u0072",
-    flagURL: '<img src="img/denmark.gif" alt="">'
-  },
-  {
-    name: "Polish Zloty",
-    abbreviation: "PLN",
-    symbol: "\u007A\u0142",
-    flagimg: '<img src="img/poland.gif" alt="">'
-  },
-  {
-    name: "Belarussian Ruble",
-    abbreviation: "BYN",
-    symbol: "\u20BD",
-    flagURL: '<img src="img/belarus.gif" alt="">'
-  }
-];
-
-
-addCurrencyBtn.addEventListener('click', function(e){
-  addCurrencyBtn.classList.toggle('open')
+document.querySelector('#add').addEventListener('click', function() {
+    return currencyConvert();
 })
+
+function currencyConvert() {
+    let select_from = document.querySelector('#select_from').value,
+        select_to = document.querySelector('#select_to').value,
+        from = document.querySelector('#input_from'),
+        to = document.querySelector('#input_to');
+
+
+
+    if (select_from == 'USD') {
+        return usd()
+    }
+    if (select_from == 'EUR') {
+        return eur()
+    }
+    if (select_from == 'RUB') {
+        return rub()
+    }
+    if (select_from == 'BYN') {
+        return byn()
+    }
+
+    function usd() {
+        let rates = {
+            EUR: 0.85,
+            RUB: 34.456,
+            BYN: 2.345,
+        }
+        let arr = Object.values(rates);
+        if (select_to == 'EUR') {
+            to.value = (arr[0] * from.value).toFixed(2);
+        } else if (select_to == 'RUB') {
+            to.value = (arr[1] * from.value).toFixed(2);
+        } else if (select_to == 'BYN') {
+            to.value = (arr[2] * from.value).toFixed(2);
+        } else {
+            to.value = from.value;
+        }
+    }
+
+    function eur() {
+        let rates = {
+            USD: 1.285,
+            RUB: 44.456,
+            BYN: 2.545,
+        }
+        let arr = Object.values(rates);
+        if (select_to == 'USD') {
+            to.value = (arr[0] * from.value).toFixed(2);
+        } else if (select_to == 'RUB') {
+            to.value = (arr[1] * from.value).toFixed(2);
+        } else if (select_to == 'BYN') {
+            to.value = (arr[2] * from.value).toFixed(2);
+        } else {
+            to.value = from.value;
+        }
+    }
+
+    function rub() {
+        let rates = {
+            USD: 0.00285,
+            EUR: 0.00156,
+            BYN: 0.0545,
+        }
+        let arr = Object.values(rates);
+        if (select_to == 'USD') {
+            to.value = (arr[0] * from.value).toFixed(2);
+        } else if (select_to == 'EUR') {
+            to.value = (arr[1] * from.value).toFixed(2);
+        } else if (select_to == 'BYN') {
+            to.value = (arr[2] * from.value).toFixed(2);
+        } else {
+            to.value = from.value;
+        }
+    }
+
+    function byn() {
+        let rates = {
+            USD: 0.585,
+            EUR: 0.4156,
+            RUB: 20.564,
+        }
+        let arr = Object.values(rates);
+        if (select_to == 'USD') {
+            to.value = (arr[0] * from.value).toFixed(2);
+        } else if (select_to == 'EUR') {
+            to.value = (arr[1] * from.value).toFixed(2);
+        } else if (select_to == 'RUB') {
+            to.value = (arr[2] * from.value).toFixed(2);
+        } else {
+            to.value = from.value;
+        }
+    }
+}
